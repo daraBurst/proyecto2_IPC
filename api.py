7a6556b1dfb597fcda859c09b62d18bb7fc3e6e6
daraBurst <monzon_rafael@hotmail.com>
2020-11-10 01:19:56 -0600

    New changes

--- api.py
+++ api.py
@@ -204,4 +204,5 @@ def remove_usr(username):
 def general():
     return render_template('contact-about.html')
 
-app.run(threaded=True, host="0.0.0.0", port=5000)
+if __name__ =='__main__':
+    app.run()
